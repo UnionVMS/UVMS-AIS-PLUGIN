aadcd3a399dd4db39839160e2801a6ccb3135229
HAV\thofan <thomas.fanto@havochvatten.se>
2019-01-03 10:28:58 +0100

    more  data from AIS

--- service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/ais/service/ProcessService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/ais/service/ProcessService.java
@@ -43,6 +43,7 @@ import eu.europa.ec.fisheries.uvms.plugins.ais.StartupBean;
 public class ProcessService {
 
     final static Logger LOG = LoggerFactory.getLogger(ProcessService.class);
+    boolean detailLog = true;
 
     @Inject
     ExchangeService exchangeService;
@@ -137,7 +138,121 @@ public class ProcessService {
         return movement;
     }
 
+
+
     private MovementBaseType parseClassBEquipmentPositionReport(String sentence) throws NumberFormatException {
+
+        if(sentence == null || sentence.trim().length() < 1) {
+            return null;
+        }
+        int sentenceLength = sentence.length();
+
+        MovementBaseType movement = new MovementBaseType();
+        String mmsi = String.valueOf(Integer.parseInt(sentence.substring(8, 38), 2));
+        movement.setMmsi(mmsi);
+        movement.setAssetId(getAssetId(mmsi));
+        movement.setReportedSpeed(parseSpeedOverGround(sentence, 46, 56));
+        movement.setPosition(getMovementPoint(parseCoordinate(sentence, 57, 85), parseCoordinate(sentence, 85, 112)));
+        movement.setReportedCourse(parseCourseOverGround(sentence, 112, 124));
+        movement.setPositionTime(getTimestamp(Integer.parseInt(sentence.substring(133, 139), 2)));
+        movement.setSource(MovementSourceType.AIS);
+        movement.setComChannelType(MovementComChannelType.NAF);
+
+        // within previous length of sentence parse
+        // type b = boolean
+        String positionAccuracyStr = sentence.substring(56,57);
+        // type = u unsigned integer
+        String trueHeadingStr = sentence.substring(124,133);
+        Integer trueHeading = parseToNumertic("TrueHeading", trueHeadingStr);
+
+
+        if(detailLog) {
+            LOG.info("positionAccuracy      " + positionAccuracyStr);
+            LOG.info("trueHeading           " + trueHeading);
+        }
+
+
+
+        String assetName = null;
+        String typeOfShipAndCargoStr =  null;
+        String dimensionToBowStr =  null;
+        String dimensionToSternStr = null;
+        String dimensionToPortStr = null;
+        String dimensionToStarBoardStr = null;
+        String positionFixType = null;
+        String RAIMFlag = null;
+        String DTE = null;
+        String assignedModeFlagStr = null;
+
+        if(sentenceLength >= 309) {
+
+            assetName = sentence.substring(143, 263);
+            movement.setAssetName(assetName);
+
+            // type = u unsigned integer
+            typeOfShipAndCargoStr = sentence.substring(263, 271);
+            Integer typeOfShipAndCargo = parseToNumertic("Type of ship and cargo", typeOfShipAndCargoStr);
+            movement.setTypeOfShipAndCargo(typeOfShipAndCargo);
+
+            // type = u unsigned integer
+            dimensionToBowStr = sentence.substring(271, 280);
+            Integer dimensionToBow = parseToNumertic("DimensionToBow", dimensionToBowStr);
+            movement.setDimensionToBow(dimensionToBow);
+
+            // type = u unsigned integer
+            dimensionToSternStr = sentence.substring(280, 289);
+            Integer dimensionToStern = parseToNumertic("DimensionToStern", dimensionToSternStr);
+            movement.setDimensionToStern(dimensionToStern);
+
+            // type = u unsigned integer
+            dimensionToPortStr = sentence.substring(289, 295);
+            Integer dimensionToPort = parseToNumertic("DimensionToPort", dimensionToPortStr);
+            movement.setDimensionToPort(dimensionToPort);
+
+            // type = u unsigned integer
+            dimensionToStarBoardStr = sentence.substring(295, 301);
+            Integer dimensionToStarBoard = parseToNumertic("DimensionToStarBoard", dimensionToStarBoardStr);
+            movement.setDimensionToStarBoard(dimensionToStarBoard);
+
+            // type = e enumerated type
+            positionFixType = sentence.substring(301, 305);
+            movement.setPositionFixType(positionFixType);
+
+            // type = b
+            RAIMFlag = sentence.substring(305, 306);
+            Boolean rf = RAIMFlag == null ? false : RAIMFlag.equals("1");
+            movement.setRAIMFlag(rf);
+
+            // type = b
+            DTE = sentence.substring(306, 307);
+            Boolean dte = DTE == null ? false : DTE.equals("1");
+            movement.setDTE(dte);
+
+
+            assignedModeFlagStr = sentence.substring(307, 308);
+            Boolean amf = assignedModeFlagStr == null ? false : assignedModeFlagStr.equals("1");
+            movement.setAssignedModeFlag(amf);
+
+
+
+            if(detailLog) {
+                LOG.info("assetName             " + assetName);
+                LOG.info("typeOfShipAndCargo    " + typeOfShipAndCargo);
+                LOG.info("dimensionToBow        " + dimensionToBow);
+                LOG.info("dimensionToStern      " + dimensionToStern);
+                LOG.info("dimensionToPort       " + dimensionToPort);
+                LOG.info("dimensionToStarBoard  " + dimensionToStarBoard);
+                LOG.info("positionFixType       " + positionFixType);
+                LOG.info("RAIMFlag              " + RAIMFlag);
+                LOG.info("DTE                   " + DTE);
+                LOG.info("AssignedModeFlag      " + assignedModeFlagStr);
+            }
+        }
+
+        return movement;
+    }
+
+    private MovementBaseType parseClassBEquipmentPositionReportOLD(String sentence) throws NumberFormatException {
         MovementBaseType movement = new MovementBaseType();
         String mmsi = String.valueOf(Integer.parseInt(sentence.substring(8, 38), 2));
         movement.setMmsi(mmsi);
@@ -210,4 +325,18 @@ public class ProcessService {
 
         return cal.getTime();
     }
+
+    private Integer parseToNumertic(String fieldName , String str) throws NumberFormatException
+    {
+        try{
+            return Integer.parseInt(str,2);
+        }
+        catch(NumberFormatException e){
+            LOG.error(fieldName + " is not numeric", e);
+            throw e;
+        }
+    }
+
+
+
 }
