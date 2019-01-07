7a475b0a463d67b364c022f6a0ccbd78ffe3512b
HAV\thofan <thomas.fanto@havochvatten.se>
2019-01-07 12:43:42 +0100

    ais type 18 info from ais

--- service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/ais/service/ProcessService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/ais/service/ProcessService.java
@@ -139,7 +139,6 @@ public class ProcessService {
     }
 
 
-
     private MovementBaseType parseClassBEquipmentPositionReport(String sentence) throws NumberFormatException {
 
         if(sentence == null || sentence.trim().length() < 1) {
@@ -148,109 +147,89 @@ public class ProcessService {
         int sentenceLength = sentence.length();
 
         MovementBaseType movement = new MovementBaseType();
+
+        // mmsi
         String mmsi = String.valueOf(Integer.parseInt(sentence.substring(8, 38), 2));
         movement.setMmsi(mmsi);
         movement.setAssetId(getAssetId(mmsi));
-        movement.setReportedSpeed(parseSpeedOverGround(sentence, 46, 56));
+
+       // String notUsed = sentence.substring(38, 46);
+
+        // speedOverGround
+        double speedOverGround = parseSpeedOverGround(sentence, 46, 56);
+        movement.setReportedSpeed(speedOverGround);
+
+        // positionaccuracy
+        Boolean positionAccuracy = parseToBoolean(sentence, 56,57);
+        movement.setPositionAccuracy(positionAccuracy);
+
+        // position  longitude latitude
         movement.setPosition(getMovementPoint(parseCoordinate(sentence, 57, 85), parseCoordinate(sentence, 85, 112)));
+
+        // course
         movement.setReportedCourse(parseCourseOverGround(sentence, 112, 124));
+
+        // trueHeading
+        String trueHeadingStr = sentence.substring(124,133);
+        Integer trueHeading = parseToNumertic("TrueHeading", trueHeadingStr);
+        movement.setTrueHeading(trueHeading);
+
+        // timestamp
         movement.setPositionTime(getTimestamp(Integer.parseInt(sentence.substring(133, 139), 2)));
+
+        // not used
+        // String notUsed2 = sentence.substring(139, 141);
+
+        // CS Unit
+        Boolean csUnit = parseToBoolean(sentence, 141,142);
+        movement.setCsUnit(csUnit);
+
+        // Display flag
+        Boolean displayFlag = parseToBoolean(sentence, 142,143);
+        movement.setDisplayFlag(displayFlag);
+
+        // DSC Flag
+        Boolean dscFlag = parseToBoolean(sentence, 143,144);
+        movement.setDscFlag(dscFlag);
+
+        // band flag
+        Boolean band = parseToBoolean(sentence, 144,145);
+        movement.setBandFlag(band);
+
+        // message22 flag
+        Boolean message22 = parseToBoolean(sentence, 145,146);
+        movement.setMessage22(message22);
+
+        // Assigned
+        Boolean assigned =  parseToBoolean(sentence, 146,147);
+        movement.setAssigned(assigned);
+
+        // Raim flag
+        Boolean raimFlag = parseToBoolean(sentence, 147,148);
+        movement.setRaimFlag(raimFlag);
+
+        //
+        String radioStatusStr = sentence.substring(148,168);
+        Integer radioStatus = parseToNumertic("RadioStatus", radioStatusStr);
+        movement.setRadioStatus(radioStatus);
+
         movement.setSource(MovementSourceType.AIS);
         movement.setComChannelType(MovementComChannelType.NAF);
 
-        // within previous length of sentence parse
-        // type b = boolean
-        String positionAccuracyStr = sentence.substring(56,57);
-        // type = u unsigned integer
-        String trueHeadingStr = sentence.substring(124,133);
-        Integer trueHeading = parseToNumertic("TrueHeading", trueHeadingStr);
-
-
-        if(detailLog) {
-            LOG.info("positionAccuracy      " + positionAccuracyStr);
-            LOG.info("trueHeading           " + trueHeading);
-        }
-
-
-
-        String assetName = null;
-        String typeOfShipAndCargoStr =  null;
-        String dimensionToBowStr =  null;
-        String dimensionToSternStr = null;
-        String dimensionToPortStr = null;
-        String dimensionToStarBoardStr = null;
-        String positionFixType = null;
-        String RAIMFlag = null;
-        String DTE = null;
-        String assignedModeFlagStr = null;
-
-        if(sentenceLength >= 309) {
-
-            assetName = sentence.substring(143, 263);
-            movement.setAssetName(assetName);
-
-            // type = u unsigned integer
-            typeOfShipAndCargoStr = sentence.substring(263, 271);
-            Integer typeOfShipAndCargo = parseToNumertic("Type of ship and cargo", typeOfShipAndCargoStr);
-            movement.setTypeOfShipAndCargo(typeOfShipAndCargo);
-
-            // type = u unsigned integer
-            dimensionToBowStr = sentence.substring(271, 280);
-            Integer dimensionToBow = parseToNumertic("DimensionToBow", dimensionToBowStr);
-            movement.setDimensionToBow(dimensionToBow);
-
-            // type = u unsigned integer
-            dimensionToSternStr = sentence.substring(280, 289);
-            Integer dimensionToStern = parseToNumertic("DimensionToStern", dimensionToSternStr);
-            movement.setDimensionToStern(dimensionToStern);
-
-            // type = u unsigned integer
-            dimensionToPortStr = sentence.substring(289, 295);
-            Integer dimensionToPort = parseToNumertic("DimensionToPort", dimensionToPortStr);
-            movement.setDimensionToPort(dimensionToPort);
-
-            // type = u unsigned integer
-            dimensionToStarBoardStr = sentence.substring(295, 301);
-            Integer dimensionToStarBoard = parseToNumertic("DimensionToStarBoard", dimensionToStarBoardStr);
-            movement.setDimensionToStarBoard(dimensionToStarBoard);
-
-            // type = e enumerated type
-            positionFixType = sentence.substring(301, 305);
-            movement.setPositionFixType(positionFixType);
-
-            // type = b
-            RAIMFlag = sentence.substring(305, 306);
-            Boolean rf = RAIMFlag == null ? false : RAIMFlag.equals("1");
-            movement.setRAIMFlag(rf);
-
-            // type = b
-            DTE = sentence.substring(306, 307);
-            Boolean dte = DTE == null ? false : DTE.equals("1");
-            movement.setDTE(dte);
-
-
-            assignedModeFlagStr = sentence.substring(307, 308);
-            Boolean amf = assignedModeFlagStr == null ? false : assignedModeFlagStr.equals("1");
-            movement.setAssignedModeFlag(amf);
-
-
-
-            if(detailLog) {
-                LOG.info("assetName             " + assetName);
-                LOG.info("typeOfShipAndCargo    " + typeOfShipAndCargo);
-                LOG.info("dimensionToBow        " + dimensionToBow);
-                LOG.info("dimensionToStern      " + dimensionToStern);
-                LOG.info("dimensionToPort       " + dimensionToPort);
-                LOG.info("dimensionToStarBoard  " + dimensionToStarBoard);
-                LOG.info("positionFixType       " + positionFixType);
-                LOG.info("RAIMFlag              " + RAIMFlag);
-                LOG.info("DTE                   " + DTE);
-                LOG.info("AssignedModeFlag      " + assignedModeFlagStr);
-            }
-        }
-
         return movement;
     }
+
+    private Boolean parseToBoolean(String sentence, int startPosInclusive, int endPosExclusive) throws NumberFormatException
+    {
+        String str = sentence.substring(startPosInclusive, endPosExclusive);
+        if(str == null ) return null;
+        return str.equals("1");
+    }
+
+
+
+
+
 
     private MovementBaseType parseClassBEquipmentPositionReportOLD(String sentence) throws NumberFormatException {
         MovementBaseType movement = new MovementBaseType();
